ff2023d7e8b71c84d3456766fdf7a9fb91e4f66c
Paulo Lopes <pocostalopes@gmail.com>
2024-11-19 01:33:03 -0300

    +1

---
+++ Difficulty - 800/A.Way Too Long Words.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+ 
+using namespace std;
+
+int main(){
+	
+	int n; //Numero de casos
+	string s;
+		
+	cin >> n;
+	
+	while(n != 0){
+		
+		cin >> s;
+		
+		if(s.length() > 10){ //Verifica o tamanho da string
+			cout << s.at(0) << s.length() - 2 << s.at(s.length() - 1) << endl; //Imprime a quantidade de caracteres entre a primeira e ultima letra					
+		}
+		else{ //tamanho menor ou igual a 10 imprime a palavra sem modificacoes
+			cout << s << endl;
+		}
+				
+		n--;	
+	}
+		
+	return 0;
+}
